bca980aa706e4a06947d8095d17ad99ca806fc96
DiegoPorta <diego@porta.com.py>
2020-09-01 18:16:57 -0400

    Inicio - adaptacion del template al proyecto

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/','FrontController@index')->name('front.index');
